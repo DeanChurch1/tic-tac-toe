332c2543cee7c00bc880ab65521cc0ca0077ffb8
DeanChurch1 <42648400+DeanChurch1@users.noreply.github.com>
2019-01-04 22:47:56 -0700

    Delete word exp.py

--- Dean Church Github/word exp.py
+++
@@ -1,9 +0,0 @@
-name = input("type in your first and last name:")
-x = name.find(" ")
-firstName = name[:x]
-lastName = name[x+1:]
-#lastName.trim(" ")
-print(firstName)
-print(lastName)
-
-display_puzzle(puzzle)
